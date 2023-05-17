b5928407562fecd8fff6850035621ccbc802d615
Raghavendra Singh Dasila <raghavdasila@gmail.com>
2023-05-17 14:31:28 +0530

    added story book essential addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,8 @@ module.exports = {
   stories: ['../**/*.story.mdx', '../**/*.story.@(js|jsx|ts|tsx)'],
   addons: [
     'storybook-dark-mode',
-    {
-      name: 'storybook-addon-turbo-build',
-      options: { optimizationLevel: 2 },
-    },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
   ],
   framework: '@storybook/react',
 };
