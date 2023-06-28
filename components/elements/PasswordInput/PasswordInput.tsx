9d145dc9670702c33832d57c3cff611d2a4424bf
BhaweshPandey <bhaweshp17@gmail.com>
2023-06-28 12:02:59 +0530

    #4 edit Icon

--- components/elements/PasswordInput/PasswordInput.tsx
+++ components/elements/PasswordInput/PasswordInput.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { PasswordInput, PasswordInputProps, Image } from '@mantine/core';
-import { Images } from '../../../public';
+import React from 'react';
+import { PasswordInput, PasswordInputProps } from '@mantine/core';
 import { PolymorphicComponentProps } from '@mantine/utils';
 import {createStyle} from './PasswordInput.style'
 
@@ -11,24 +10,15 @@ interface BaseSearchInput extends PolymorphicComponentProps<'input', PasswordInp
 export const BasePasswordInput = (
   props: BaseSearchInput
 ) => {
-  const [toggle, setToggle] = useState(false);
   const useStyles = createStyle('en');
   const { classes } = useStyles();
 
   return (
     <PasswordInput
       {...props}
-      onClick={() => setToggle(!toggle)}
       className={classes.PasswordInput}
       placeholder={props.placeholder}
       radius="xl"
-      visibilityToggleIcon={({ reveal }) =>
-        reveal ? (
-          <Image src={Images.dark_eye_icon} alt="dark_eye_icon" width="18px" height='13px' />
-        ) : (
-          <Image src={Images.light_eye_icon} alt="light_eye_icon" width="18px" height='13px' />
-        )
-      }
     />
   );
 };
