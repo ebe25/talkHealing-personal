5c63403b6d6f60042c36a7343ac9cb8c6e96a5c1
BhaweshPandey <bhaweshp17@gmail.com>
2023-07-20 13:31:00 +0530

    folder name change

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import { Welcome } from '../components/modules/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/elements/ColorSchemeToggle/ColorSchemeToggle';
 
 import { useStores } from "@/models";
-import TermsAndCondition from './terms_and_condition';
-import PrivacyPolicy from './privacy_policy';
-import CookiePolicy from './cookie_policy';
+import TermsAndCondition from './terms-and-condition';
+import PrivacyPolicy from './privacy-policy';
+import CookiePolicy from './cookie-policy';
 
 export default function HomePage() {
   const {userStore}=useStores()
