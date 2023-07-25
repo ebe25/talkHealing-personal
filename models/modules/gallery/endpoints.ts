0eab27b4fa0f44f413e60021332cb939d139e840
Raghavendra Singh Dasila <raghavdasila@outlook.com>
2023-07-25 14:48:21 +0530

    WIP type patches

--- models/modules/gallery/endpoints.ts
+++ models/modules/gallery/endpoints.ts
@@ -3,28 +3,28 @@ import * as SCHEMAS from "./schemas"
 
 const TRANSFORMERS = {
   // ------------------------ gallery items transformer
-  galleryItemTransformer: (data) => ({
+  galleryItemTransformer: (data:Record<string,any>) => ({
     ...data,
     created_on: new Date(data.created_on),
     edited_on: new Date(data.edited_on),
   }),
-  galleryItemsTransformer: (data) => ({
+  galleryItemsTransformer: (data:Record<string,any>) => ({
     ...data,
     results: data.results.map(TRANSFORMERS.galleryItemTransformer),
   }),
   // ------------------------ gallery transformer
-  galleryImagesTransformer: (data) => ({
+  galleryImagesTransformer: (data:Record<string,any>) => ({
     ...data,
     created_on: new Date(data.created_on),
     edited_on: new Date(data.edited_on),
   }),
-  galleryTransformer: (data) => ({
+  galleryTransformer: (data:Record<string,any>) => ({
     ...data,
     created_on: new Date(data.created_on),
     edited_on: new Date(data.edited_on),
     gallery_image: data.gallery_image.map(TRANSFORMERS.galleryImagesTransformer),
   }),
-  galleriesTransformer: (data) => ({
+  galleriesTransformer: (data:Record<string,any>) => ({
     ...data,
     results: data.results.map(TRANSFORMERS.galleryTransformer),
   }),
