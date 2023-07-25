0b31413ad210a6a7777e3158d87be4bbad9ee6a2
ipadhalni <sachinpadhalni9@gmail.com>
2023-07-25 14:59:45 +0530

    #29 PR issues fixed

--- components/modules/Settings/Settings.tsx
+++ components/modules/Settings/Settings.tsx
@@ -1,68 +1,64 @@
 import React from 'react';
-import { Box, Divider, Flex, Select, Stack, Switch  } from '@mantine/core';
+import { Box, Divider, Flex, Select, Stack, Switch } from '@mantine/core';
 
 import useStyles from './Setting.styles';
 import { BaseText } from '@/components/elements/BaseText/BaseText';
 import { typography } from '@/themes/Mantine/typography';
 import { useStores } from '@/models';
-import  Router  from 'next/router';
+import Router from 'next/router';
 import { languageDetails } from '@/models/modules/i18n/schema';
+import { TxKeyPath } from '@/i18n';
 
 export const Settings = () => {
   const { i18nStore } = useStores();
   const { classes } = useStyles();
+
+  const NOTIFICATION_CONTANT = [
+    {
+      label: 'profile.setting.push' as TxKeyPath,
+    },
+    {
+      label: 'profile.setting.push' as TxKeyPath,
+    },
+    {
+      label: 'profile.setting.push' as TxKeyPath,
+    },
+  ];
+
   return (
     <Box className={classes.container}>
-        <BaseText
-            txtkey="profile.setting.changeLanguage"
-            style={typography.headings[i18nStore.getCurrentLanguage()].h5}
-        />
-        <Select
-            mt={"10px"}
-            placeholder="English"
-            onChange={async (value) => {
-              await i18nStore.setAppLanguage(value);
-              Router.reload();
-            }}
-            defaultValue={i18nStore.getCurrentLanguage()}
-            data={[
-              { value: "en", label: `${languageDetails.en.localName}` },
-              { value: "ar", label: `${languageDetails.ar.localName}` },
-            ]}
-        />
-        <Divider
-            my={"xl"}
-        />
+      <BaseText
+        txtkey="profile.setting.changeLanguage"
+        style={typography.headings[i18nStore.getCurrentLanguage()].h5}
+      />
+      <Select
+        mt={'10px'}
+        placeholder="English"
+        onChange={async (value) => {
+          await i18nStore.setAppLanguage(value);
+          Router.reload();
+        }}
+        defaultValue={i18nStore.getCurrentLanguage()}
+        data={[
+          { value: 'en', label: `${languageDetails.en.localName}` },
+          { value: 'ar', label: `${languageDetails.ar.localName}` },
+        ]}
+      />
+      <Divider my={'xl'} />
 
-        <Stack>
-          <Flex
-            justify={"space-between"}
-            align={"center"}
-          >
-            <BaseText
-                txtkey='profile.setting.push'
-            /> 
-            <Switch onLabel="ON" offLabel="OFF" />
-          </Flex>
-          <Flex
-            justify={"space-between"}
-            align={"center"}
-          >
-            <BaseText
-                txtkey='profile.setting.emailNotification'
-            /> 
-            <Switch onLabel="ON" offLabel="OFF" />
-          </Flex>
-          <Flex
-            justify={"space-between"}
-            align={"center"}
-          >
-            <BaseText
-                txtkey='profile.setting.smsNotification'
-            /> 
-            <Switch onLabel="ON" offLabel="OFF" />
-          </Flex>
-        </Stack>
+      <Stack>
+        {NOTIFICATION_CONTANT.map((item, id) => {
+          return (
+            <Flex 
+              justify={'space-between'} 
+              align={'center'}
+            >
+              <BaseText txtkey={item.label} />
+              <Switch onLabel="ON" offLabel="OFF" />
+            </Flex>
+          );
+        })}
+      </Stack>
     </Box>
   );
 };
