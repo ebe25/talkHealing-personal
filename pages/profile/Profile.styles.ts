fbc16b5a49b1d8ec298c3ef8322e81495e87ae09
ipadhalni <sachinpadhalni9@gmail.com>
2023-07-25 10:54:09 +0530

    #29 PR issues has been fixed

--- pages/profile/Profile.styles.ts
+++ pages/profile/Profile.styles.ts
@@ -1,16 +1,58 @@
 import { createStyles } from '@mantine/core';
-import {typography } from "@/themes/Mantine/typography";
+import { typography } from '@/themes/Mantine/typography';
 
-export default createStyles((theme) => ({
-  title: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    marginTop: "50px",
-    ...typography.headings.en.h2,
-  },
-  deleteAndLogout:{
-    marginTop: "",
-    [theme.fn.smallerThan('xs')]: {
-      marginTop: "10px",
+export const createStyle = () => {
+
+  return createStyles((theme) => ({
+    tab: {
+      ...theme.fn.focusStyles(),
+      color: theme.colors.gray[6] ,
+      marginTop: "30px",
+      width:"99px",
+      height:"38px",
+      borderRadius: "33px",
+      border:"none",
+      backgroundColor: "transparent",
+      ...typography.buttonText.en.b4,
+      padding: "10px 18px 10px 18px",
+      cursor: 'pointer',
+      fontSize: theme.fontSizes.sm,
+      display: 'flex',
+      alignItems: 'center',
+  
+      '&:hover': {
+          backgroundColor: theme.colors.blue[9],
+        borderColor: "none",
+        color: theme.colors.blue[7],
+      },
+      '&:disabled': {
+          opacity: 0.5,
+          cursor: 'not-allowed',
+      },
+  
+      '&:not(:first-of-type)': {
+        borderLeft: 0,
+      },
+      '&[data-active]': {
+        backgroundColor: theme.colors.blue[9],
+        borderColor: "none",
+        color: theme.colors.blue[7],
+      },
     },
-  }
-}));
+    tabsList: {
+      display: 'flex',
+      border:"none"
+    },
+    title: {
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      marginTop: '50px',
+      ...typography.headings.en.h2,
+    },
+    deleteAndLogout: {
+      marginTop: '',
+      [theme.fn.smallerThan('xs')]: {
+        marginTop: '10px',
+      },
+    },
+  }));
+};
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -6,7 +6,7 @@ import { typography } from '../../themes/Mantine/typography';
 // components
 import { BaseText } from '@/components/elements/BaseText/BaseText';
 //styles import
-import useStyles from './Profile.styles';
+import { createStyle } from './Profile.styles';
 import { Account } from '@/components/modules/Account/Account';
 import { Address } from '@/components/modules/Address/Address';
 import { Settings } from '@/components/modules/Settings/Settings';
@@ -17,76 +17,8 @@ import { LogOutModal } from '@/components/modules/Modals/ProfileModals/LogOutMod
 import { DeleteAccountModal } from '@/components/modules/Modals/ProfileModals/DeleteAccountModal/DeleteAccountModal';
 //stores
 
-function StyledTabs(props: TabsProps) {
-    return (
-      <Tabs
-        unstyled
-        styles={(theme) => ({
-            tab: {
-                ...theme.fn.focusStyles(),
-                color: theme.colors.gray[6] ,
-                marginTop: "30px",
-                width:"99px",
-                height:"38px",
-                borderRadius: "33px",
-                border:"none",
-                backgroundColor: "transparent",
-                ...typography.buttonText.en.b4,
-                // border: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[4]}`,
-                // padding: `${theme.spacing.xs} `,
-                padding: "10px 18px 10px 18px",
-                cursor: 'pointer',
-                fontSize: theme.fontSizes.sm,
-                display: 'flex',
-                alignItems: 'center',
-            
-                '&:hover': {
-                    backgroundColor: theme.colors.blue[9],
-                  borderColor: "none",
-                  color: theme.colors.blue[7],
-                },
-                '&:disabled': {
-                    opacity: 0.5,
-                    cursor: 'not-allowed',
-                },
-            
-                '&:not(:first-of-type)': {
-                  borderLeft: 0,
-                },
-            
-                // '&:first-of-type': {
-                //   borderTopLeftRadius: theme.radius.md,
-                //   borderBottomLeftRadius: theme.radius.md,
-                // },
-            
-                // '&:last-of-type': {
-                //   borderTopRightRadius: theme.radius.md,
-                //   borderBottomRightRadius: theme.radius.md,
-                // },
-            
-                '&[data-active]': {
-                  backgroundColor: theme.colors.blue[9],
-                  borderColor: "none",
-                  color: theme.colors.blue[7],
-                },
-              },
-  
-          tabIcon: {
-            marginRight: theme.spacing.xs,
-            display: 'flex',
-            alignItems: 'center',
-          },
-  
-          tabsList: {
-            display: 'flex',
-          },
-        })}
-        {...props}
-      />
-    );
-  }
-
 const Profile = () => {
+  const useStyles=createStyle()
   const { classes } = useStyles();
   const theme = useMantineTheme();
   const [opened, { open, close }] = useDisclosure(false);
@@ -97,7 +29,10 @@ const Profile = () => {
     <Container maw={'1000px'}>
       <BaseText className={classes.title} txtkey="profile.heading" />
 
-      <StyledTabs defaultValue="Account" >
+      <Tabs classNames={{
+        tab:classes.tab,
+        tabsList:classes.tabsList
+      }} defaultValue="Account" >
       <Flex
         justify={"space-between"}
         align={"baseline"}
@@ -140,7 +75,7 @@ const Profile = () => {
         <Tabs.Panel value="Account"> <Account/> </Tabs.Panel>
         <Tabs.Panel value="Addresses"> <Address/> </Tabs.Panel>
         <Tabs.Panel value="Settings"><Settings/></Tabs.Panel>
-      </StyledTabs>
+      </Tabs>
     </Container>
     <LogOutModal
       onClose={close}
