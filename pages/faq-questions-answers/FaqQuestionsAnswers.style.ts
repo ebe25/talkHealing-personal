7795b35841f9f5090eba0dd94e8654053a279ce2
BhaweshPandey <bhaweshp17@gmail.com>
2023-07-27 15:05:12 +0530

    faq api

--- pages/faq-questions-answers/FaqQuestionsAnswers.style.ts
+++ pages/faq-questions-answers/FaqQuestionsAnswers.style.ts
@@ -1,16 +1,16 @@
 import { createStyles } from '@mantine/core';
-import {typography } from "@/themes/Mantine/typography";
+import { typography } from "@/themes/Mantine/typography";
 
 export default createStyles((theme) => ({
-    termsAndConditionBox : {
+    faqContainer: {
         padding: "50px",
         width: "100%",
         height: "100%"
     },
-    faqQuestionsAnswersBox :{
-        marginTop :'100px',
+    faqQuestionsAnswersBox: {
+        marginTop: '100px',
         [theme.fn.smallerThan('md')]: {
             marginTop: 50,
-          },
+        },
     }
 }));
--- pages/faq-questions-answers/index.tsx
+++ pages/faq-questions-answers/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Accordion, Box, Container, Flex, useMantineTheme } from '@mantine/core';
+import React, { useEffect, useState } from 'react'
+import { Accordion, Box, Center, Container, Flex, Loader, Stack, useMantineTheme } from '@mantine/core';
 import { BaseText } from '@/components/elements/BaseText/BaseText';
 import { typography } from '@/themes/Mantine/typography';
 import useStyles from './FaqQuestionsAnswers.style';
@@ -19,37 +19,100 @@ export const AccordionBox = (props: any) => {
 
 export const FaqQuestionsAnswers = () => {
     const { classes } = useStyles();
-    const { i18nStore, userStore } = useStores();
+    const { i18nStore, globalsStore } = useStores();
     const theme = useMantineTheme();
+    const [faqCategory, setFaqCategory] = useState<any>([]);
+    const [faqLoading, setFaqLoading] = useState<boolean>(true);
+    const [searchText, setSearchText] = useState<string>("");
+    const [faq, setFaq] = useState<any>({});
+
+    const findFaqCategory = (categoryList: Array<string>) => {
+        let faqDataObject: any = {}
+        let faqData: any = globalsStore.faqData?.results
+        if (categoryList.length > 0) {
+            categoryList.forEach((element: any) => {
+                faqDataObject[element['en']] = []
+            })
+        }
+        faqData.forEach((item: any) => {
+            faqDataObject[item.faq_category.en].push(item)
+        })
+        setFaq({ ...faqDataObject })
+        setFaqLoading(false)
+    };
+
+    useEffect(() => {
+        globalsStore.getFAQCategory().then((res) => {
+            if (res.ok) {
+                let faqCategoryName: any = [];
+                globalsStore.faqCategoryData?.results.map((item) => {
+                    faqCategoryName.push(item.name);
+                });
+                setFaqCategory([...faqCategoryName]);
+                if (faqCategoryName.length === globalsStore.faqCategoryData?.count)
+                    globalsStore.getFAQ().then((res) => {
+                        if (res.ok) {
+                            findFaqCategory(faqCategoryName)
+                        }
+                    });
+            }
+        });
+    }, []);
 
     return (
         <Container maw='1440px'>
-            <Box className={classes.termsAndConditionBox}>
+            <Box className={classes.faqContainer}>
                 {/* Heading */}
                 <Flex gap={'25px'} wrap={'wrap'} justify={'space-between'}>
                     <BaseText color={theme.colors.black[5]} style={typography.headings[i18nStore.getCurrentLanguage()].h3} txtkey={'frequentlyAskedQuestions.heading'} />
-                    <Input variant={'filled'} radius={'8px'} w={'500px'} component={"input"} type='text' style_variant={'inputText1'} placeholder={`${translate('frequentlyAskedQuestions.search')}`} />
+                    <Input variant={'filled'} radius={'8px'} w={'500px'} onChange={(e) => setSearchText(e.target.value)} component={"input"} type='text' style_variant={'inputText1'} placeholder={`${translate('frequentlyAskedQuestions.search')}`} />
                 </Flex>
                 {/* FaqQuestionsAnswers */}
-                <Box className={classes.faqQuestionsAnswersBox} >
-                    {[1, 2].map(() =>
-                        <Flex gap={'10px'} wrap={'wrap'} justify={'space-between'}>
-                            <BaseText mt={'20px'} color={theme.colors.black[5]} style={typography.headings[i18nStore.getCurrentLanguage()].h6} txtkey={'frequentlyAskedQuestions.loremIpsum'} />
-                            <Box w={'695px'}>
-                                {[1, 2].map(() =>
-                                    <AccordionBox w={'100%'}>
-                                        <Accordion.Control>
-                                            <BaseText color={theme.colors.black[5]} style={typography.headings[i18nStore.getCurrentLanguage()].h7} txtkey={'frequentlyAskedQuestions.faqQuestion'} />
-                                        </Accordion.Control>
-                                        <Accordion.Panel>
-                                            <BaseText color={theme.colors.black[2]} style={typography.headings[i18nStore.getCurrentLanguage()].p2} txtkey={'frequentlyAskedQuestions.faqAns'} />
-                                        </Accordion.Panel>
-                                    </AccordionBox>
-                                )}
-                            </Box>
-                        </Flex>
-                    )}
-                </Box>
+                <Stack spacing="xl" className={classes.faqQuestionsAnswersBox} >
+                    {
+                        !faqLoading ?
+                            faqCategory.map((item: any, id: any) => {
+                                let faqQuestionAnswerData: Array<any> = faq[item['en']]
+                                let filterData: Array<any> = faqQuestionAnswerData.filter((element) => {
+                                    if (element.question && element.question[i18nStore.getCurrentLanguage()].includes(searchText)) {
+                                        return element
+                                    }
+                                })
+                                if (filterData.length > 0 && filterData[0].faq_category.en === item['en'])
+                                    return (
+                                        <Flex key={id} gap={'10px'} wrap={'wrap'} justify={'space-between'}>
+                                            <BaseText mt={'20px'} color={theme.colors.black[5]} style={typography.headings[i18nStore.getCurrentLanguage()].h6} >
+                                                {item[i18nStore.getCurrentLanguage()]}
+                                            </BaseText>
+                                            <Box w={'695px'}>
+                                                {faqQuestionAnswerData.filter((element) => {
+                                                    if (element.question[i18nStore.getCurrentLanguage()].includes(searchText)) {
+                                                        return element
+                                                    }
+                                                }).map((item, id) => (
+                                                    <AccordionBox key={id} w={'100%'}>
+                                                        <Accordion.Control>
+                                                            <BaseText color={theme.colors.black[5]} style={typography.headings[i18nStore.getCurrentLanguage()].h7} >
+                                                                {item.question ? item.question[i18nStore.getCurrentLanguage()] : ""}
+                                                            </BaseText>
+                                                        </Accordion.Control>
+                                                        <Accordion.Panel>
+                                                            <BaseText color={theme.colors.black[2]} style={typography.headings[i18nStore.getCurrentLanguage()].p2}  >
+                                                                {item.answer ? item.answer[i18nStore.getCurrentLanguage()] : ""}
+                                                            </BaseText>
+                                                        </Accordion.Panel>
+                                                    </AccordionBox>
+                                                ))}
+                                            </Box>
+                                        </Flex>
+                                    );
+                                return null
+                            }) :
+                            <Center>
+                                <Loader size="xl" />
+                            </Center>
+                    }
+                </Stack>
             </Box>
         </Container>
     )
