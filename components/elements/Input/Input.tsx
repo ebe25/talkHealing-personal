893e2ccc81a4a779ef8f08b8e0155e9be395f883
BhaweshPandey <bhaweshp17@gmail.com>
2023-07-28 11:26:47 +0530

    #25 use i18nStore

--- components/elements/Input/Input.tsx
+++ components/elements/Input/Input.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { TextInput } from '@mantine/core';
 import { createStyle } from './Input.style';
 import { BaseTextInputProps } from './Input.style';
-import { useStores } from '@/models';
 
 
 export const Input = (props: BaseTextInputProps) => {
-  const { i18nStore } = useStores();
-  const useStyles = createStyle(i18nStore.getCurrentLanguage() as any, props);
+  const useStyles = createStyle(props);
   const { classes } = useStyles();
 
   return (
