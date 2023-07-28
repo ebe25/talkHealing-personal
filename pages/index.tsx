ab3a613c71c228cde0df4073be286a8ddaac1662
ipadhalni <sachinpadhalni9@gmail.com>
2023-07-28 10:58:08 +0530

    #42 index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
-import Profile from './profile';
-import Login from './login';
+// import Profile from './profile';
+// import Login from './login';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+import { Box, Center, Container, Loader } from '@mantine/core';
 
 export default function HomePage() {
-  
+  const router = useRouter()
+  useEffect(()=>{
+    //later check if logged in here
+    router.push("/login")
+    return ()=>{
+
+    }
+  },[])
   return (
     <>
-      <Profile/>
+      {/* <Profile/> */}
     {/* <Login /> */}
+    <Box >
+      <Container>
+        <Center h="100vh">
+          <Loader size="xl"/>
+        </Center>
+      </Container>
+    </Box>
       {/* <Welcome /> */}
       {/* <ColorSchemeToggle /> */}
     </>
