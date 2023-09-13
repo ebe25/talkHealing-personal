a1ff0618adad8bca1a656723c14ab2a7e92e8b8e
BhaweshPandey <bhaweshp17@gmail.com>
2023-09-13 17:25:30 +0530

    #65 header

---
+++ components/modules/Header/Header.style.ts
@@ -0,0 +1,59 @@
+import { createStyles } from '@mantine/core';
+import { useStores } from '@/models';
+
+export const createStyle = () => {
+    const { i18nStore } = useStores()
+
+    return createStyles((theme) => ({
+        containerBox: {
+            height: "78px",
+            width: "100%",
+            backgroundColor: theme.colors.blue[4],
+            margin: "0px",
+        },
+        container: {
+            justifyContent: "center",
+            height: "78px",
+            width: "100%",
+            padding: "0px 10px",
+            alignItems: "center",
+            gap: "20px",
+            [theme.fn.smallerThan('xs')]: {
+                display: "none"
+            },
+        },
+        categories: {
+            width: "140px",
+            border: `1px solid ${theme.colors.blue[9]}`,
+            borderRadius: "20px",
+            justifyContent: "center",
+            padding: "5px",
+            alignItems: "center",
+            cursor: "pointer",
+            gap: "10px"
+        },
+        notification: {
+            borderLeft: "2px solid #FFFFFF",
+            height: "28px",
+            paddingLeft: "10px",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        picBox: {
+            marginTop: "5px",
+            justifyContent: "center",
+            gap: "8px"
+        },
+        mobileMenu: {
+            justifyContent: "center",
+            height: "78px",
+            width: "100%",
+            padding: "0px 10px",
+            alignItems: "center",
+            gap: "20px",
+            [theme.fn.largerThan('xs')]: {
+                display: "none"
+            },
+        },
+    }))
+}
---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Flex, Image, Box } from '@mantine/core';
+import { useMantineTheme } from '@mantine/core';
+import { Images } from '../../../public/index';
+import { createStyle } from "./Header.style";
+import { BaseText } from '@/components/elements/BaseText/BaseText';
+import { useStores } from '@/models';
+import { typography } from '@/themes/Mantine/typography';
+import { IconArrowBadgeDown } from '@tabler/icons-react';
+import { SearchInput } from '@/components/elements/SearchInput/SearchInput';
+import { translate } from '../../../i18n';
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Button, Group } from '@mantine/core';
+
+function Header() {
+    const theme = useMantineTheme();
+    const { i18nStore, userStore } = useStores();
+    const useStyles = createStyle();
+    const { classes } = useStyles();
+    const [opened, { open, close }] = useDisclosure(false);
+    return (
+        <Box className={classes.containerBox}>
+            <Flex className={classes.container}>
+                <Image src={Images.logo} width={86} height={22} />
+                <Flex className={classes.categories}>
+                    <BaseText style={typography.paragraph[i18nStore.getCurrentLanguage()].p3} c={"white"} txtkey={"header.allCategories"} />
+                    <IconArrowBadgeDown color='white' />
+                </Flex>
+                <SearchInput w={440} placeholder={`${translate("frequentlyAskedQuestions.search")}`} />
+                <Image src={Images.shop_icon} width={20} height={20} />
+                <Image src={Images.chat} width={20} height={20} />
+                <Image src={Images.tiptop} width={14} height={17} />
+                <Flex className={classes.notification}>
+                    <Image src={Images.notification_icon} width={19} height={19} />
+                </Flex>
+                <Image src={Images.pic} width={34} height={34} />
+                <Box>
+                    <BaseText c={theme.colors.blue[0]} style={typography.paragraph[i18nStore.getCurrentLanguage()].p7} txtkey={"header.yourAccount"} />
+                    <Flex className={classes.picBox}>
+                        <BaseText c={theme.white} style={typography.paragraph[i18nStore.getCurrentLanguage()].p3} txtkey={"header.name"} />
+                        <Image mt={8} src={Images.down_arrow} width={10} height={6} />
+                    </Flex>
+                </Box>
+            </Flex>
+            <Flex className={classes.mobileMenu}>
+                <Image src={Images.logo} width={86} height={22} />
+                <SearchInput w={440} placeholder={`${translate("frequentlyAskedQuestions.search")}`} />
+                <Drawer opened={opened} onClose={close} title="LOGO">
+                    {/* Drawer content */}
+                </Drawer>
+
+                <Group position="center">
+                    {/* <Button onClick={open}>Open Drawer</Button> */}
+                    <Image onClick={open} src={Images.menu_icon} width={25} height={25} />
+                </Group>
+            </Flex>
+        </Box >
+    )
+}
+
+export default Header
