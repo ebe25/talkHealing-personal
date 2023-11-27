89b305da1dcdd3ae987b864dab0197bac071561e
student-vedansh <35373105+ebe25@users.noreply.github.com>
2023-11-27 11:10:42 +0530

    fixed: Placedholder alignment in the input fields login/signup

--- components/elements/Input/Input.style.ts
+++ components/elements/Input/Input.style.ts
@@ -39,7 +39,7 @@ export const createStyle = (
     },
     innerInput: {
       textAlign: i18nStore.isRTL ? "right" : "left",
-      padding: " 0px 40px"
+      padding: " 0px 10px"
     }
   }
   ))
