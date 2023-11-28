ed72c0b977ab2d93f06e855b311465e8095940e8
student-vedansh <35373105+ebe25@users.noreply.github.com>
2023-11-28 11:54:08 +0530

    signup logo sorted

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -44,7 +44,7 @@ export const SignUp = (props: signUpProps) => {
     <Container maw={'1400px'}>
       <Grid className={classes.container} gutter="100px" m={0}>
         <Grid.Col sm={12} xs={12} md={8} lg={7} xl={7}>
-          <Image w={'100%'} src={props.img ? props.img : Images.login_icon} alt="login_icon" />
+          <Image w={'100%'} src={props.img ? props.img : Images.public_health} alt="login_icon" />
         </Grid.Col>
         <Grid.Col sm={12} xs={12} md={4} lg={5} xl={5}>
           {/* Signup Fragment and Email Otp Fragment and Add number Fragment and Number Otp Fragment */}
